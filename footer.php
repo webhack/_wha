1a48e1c61e2f7c011d9f220aebae67eb51c460d9
Alexander Chumak <o.chumak@svitla.com>
2018-09-14 20:02:53 +0300

    WP init

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package _wha
+ */
+?>
+<?php wp_footer(); ?>
+
+</body>
+</html>
